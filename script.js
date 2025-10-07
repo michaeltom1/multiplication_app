50a581f7e9001b6e7dc71582872dc78a678ebdbd
michaeltom1 <mic81070@gmail.com>
2025-10-07 14:54:31 +0100

    added Offline Capability

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
 document.addEventListener("DOMContentLoaded", () => {
   // Get DOM elements
   const tableSelect = document.getElementById("tableSelect");
